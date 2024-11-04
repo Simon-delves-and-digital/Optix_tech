44e83cd92ffc93fd8dd9dc4860ce4faa2b969f4f
Simon D <simon.delves@and.digital>
2024-11-04 18:22:52 +0000

    added loadData functtion to call BE apis for data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { useRef, useState, Children} from 'react';
+import { useRef, useState, Children, useEffect} from 'react';
 import { easeIn, easeOut } from "polished";
 import { useBoolean } from "react-use";
 import { createReducer }from "@reduxjs/toolkit"
+import { getCompanies, getMovies } from './API/api';
+const SERVER_URL=import.meta.env.VITE_SERVER_URL
 
 // TODO: use https://giddy-beret-cod.cyclic.app/movieCompanies
 const mockMovieCompanyData: any = [
@@ -14,37 +16,72 @@ const mockMovieData: any = [
   {id: "2", reviews: [5,7,3,4,1,6,3], title: "Mock Test Film", filmCompanyId: "1", cost : 6234, releaseYear: 2006},
 ];
 
-export const App = () =>  {
 
-  const movieLength = useRef(mockMovieData.length);
+export const App = () =>  {
+  const [movies, setMovies] = useState([]);
+  const [filmCompanies, setFilmCompanies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
   const [selectedMovie, setSelectedMovie] = useState(0); 
 
+  const loadData = async () => {
+    setError("");
+    setLoading(true)
+    
+    let fetchedMovies = await getMovies()
+    let fetchedCompanies = await getCompanies()
+
+    if(fetchedMovies && fetchedCompanies) {
+      setMovies(fetchedMovies)
+      setFilmCompanies(fetchedCompanies)
+    }
+    else
+    {
+      if (!fetchedMovies) {
+        setMovies([])
+        setError("Failed to load film data")
+      }
+      if (!fetchedCompanies){
+        setFilmCompanies([])
+        setError("Failed to load film company data")
+      }
+    }
+
+    setLoading(false)
+}
+
   const refreshButton = (buttonText: any) => {
-    if (mockMovieCompanyData) {
-      return <button>{buttonText}</button>
+    if (!loading) {
+      return <button onClick={loadData}>{buttonText}</button>
     } else {
       return <p>No movies loaded yet</p>
     }   
   };
+  useEffect(() => {
+    loadData()
+  }, [SERVER_URL]);
+
 
   return (
     <div>
       <h2>Welcome to Movie database!</h2>
       {refreshButton("Refresh")}
-      <p>Total movies displayed {movieLength.current}</p>
+      {error &&<p>{error}</p>}
+      <p>Total movies displayed {movies.length}</p>
       <span>Title - Review - Film Company</span>
       <br/>
-      {mockMovieData.map((movie: any) => 
+      {movies.map((movie: any) => 
         <span onClick={() => {setSelectedMovie(movie)}}>
           {movie.title}{" "}
           {movie.reviews.reduce((acc: any, i: any) => (acc + i)/movie.reviews.length, 0)?.toString().substring(0, 3)}{" "}
-          {mockMovieCompanyData.find((f: any) => f.id === movie.filmCompanyId)?.name}
+          {/* {filmCompanies.find((f: any) => f.id === movie.filmCompanyId)?.name} */}
           <br/>
         </span>
       )}
       <br/>
       <div>
-       {selectedMovie ? selectedMovie.title as any ? "You have selected " +  selectedMovie.title  as any : "No Movie Title" : "No Movie Seelcted"}
+       {/* {selectedMovie ? selectedMovie.title as any ? "You have selected " +  selectedMovie.title  as any : "No Movie Title" : "No Movie Selcted"} */}
        {selectedMovie && <p>Please leave a review below</p> }
        {selectedMovie && 
         <form onSubmit={() => {}}>
